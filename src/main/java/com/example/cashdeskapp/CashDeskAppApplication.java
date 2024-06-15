2fceb9c63e9ab909239c29f5cdee0c08bf301d1d
Pepi <petranka_popova_@abv.bg>
2024-06-15 08:31:35 +0300

    Initial commit

---
+++ src/main/java/com/example/cashdeskapp/CashDeskAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashdeskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashDeskAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashDeskAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cashdeskapp/CashDeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashdeskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashDeskAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
